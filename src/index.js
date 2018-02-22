c642d22079f253d9d3dca7898d647822694dafb6
igor-provotorov <garynya@yandex.ru>
2018-02-22 19:25:57 +0300

    love-triangle/feature: complete task

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let result = 0;
+
+    for (let n = 0; n < preferences.length; n++) {
+        if (preferences[n] != n + 1) {
+
+            if (preferences[preferences[n] - 1] != n + 1 && preferences[preferences[n] - 1] != preferences[n]) {
+
+                if (preferences[preferences[preferences[n] - 1] - 1] == n + 1) {
+                    result++;
+                }
+            }
+        }
+    }
+    return result / 3;
 };
